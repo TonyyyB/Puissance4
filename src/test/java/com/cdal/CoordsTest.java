cf18f5e5d693f560c7cf313a6e823abd51e017cd
Julien Bailly <julien.bailly2@etu.univ-orleans.fr>
2024-06-16 12:19:44 +0200

    test fin

--- src/test/java/com/cdal/CoordsTest.java
+++ src/test/java/com/cdal/CoordsTest.java
@@ -1,39 +1,44 @@
 package test.java.com.cdal;
 
 import main.java.com.cdal.Coords;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CoordsTest {
 
+    private Coords c1;
+    private Coords c2;
+
+    @BeforeEach
+    public void setUp() {
+        c1 = new Coords(2, 3);
+        c2 = new Coords(3, 4);
+    }
+
     @Test
     public void testConstructorAndGetters() {
-        Coords coords = new Coords(3, 4);
-        assertEquals(3, coords.getL());
-        assertEquals(4, coords.getC());
+        assertEquals(2, c1.getL());
+        assertEquals(3, c1.getC());
     }
 
     @Test
     public void testPlusMethodWithTwoCoords() {
-        Coords c1 = new Coords(1, 2);
-        Coords c2 = new Coords(3, 4);
         Coords result = c1.plus(c1, c2);
-        assertEquals(4, result.getL());
-        assertEquals(6, result.getC());
+        assertEquals(5, result.getL());
+        assertEquals(7, result.getC());
     }
 
     @Test
     public void testMoinsMethodWithTwoCoords() {
-        Coords c1 = new Coords(5, 7);
-        Coords c2 = new Coords(2, 3);
-        Coords result = c1.moins(c1, c2);
-        assertEquals(3, result.getL());
-        assertEquals(4, result.getC());
+        Coords c3 = new Coords(5, 7);
+        Coords result = c1.moins(c3, c2);
+        assertEquals(2, result.getL());
+        assertEquals(3, result.getC());
     }
 
     @Test
     public void testPlusMethodWithCoordsAndInts() {
-        Coords c1 = new Coords(2, 3);
         Coords result = c1.plus(c1, 3, 4);
         assertEquals(5, result.getL());
         assertEquals(7, result.getC());
@@ -41,8 +46,8 @@ public class CoordsTest {
 
     @Test
     public void testMoinsMethodWithCoordsAndInts() {
-        Coords c1 = new Coords(5, 7);
-        Coords result = c1.moins(c1, 2, 3);
+        Coords c3 = new Coords(5, 7);
+        Coords result = c1.moins(c3, 2, 3);
         assertEquals(3, result.getL());
         assertEquals(4, result.getC());
     }
